a24b0d2d7457ad18dd07d43c36f2d24650b7cc34
David Cruz <david@codebycruz.com>
2025-09-02 11:07:57 -0700

    Combine styles
    Fixes #1

--- packages/dynote-playground/src/index.tsx
+++ packages/dynote-playground/src/index.tsx
@@ -3,8 +3,10 @@ import { DynoteEditor } from '@dynote/editor';
 
 function App() {
   return (
-    <div className="p-8 max-w-4xl mx-auto">
-      <DynoteEditor />
+    <div className="dark min-h-screen bg-background text-foreground">
+      <div className="p-8 max-w-4xl mx-auto">
+        <DynoteEditor />
+      </div>
     </div>
   );
 }
@@ -13,7 +15,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@dynote/editor/styles';
-import '@dynote/editor/tailwind';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
