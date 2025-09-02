ab570ded2515b71e59112c1dad5bb9fcf62fd44c
David Cruz <david@codebycruz.com>
2025-09-01 18:01:27 -0700

    Change package names

--- packages/dynote-playground/src/frontend.tsx
+++ packages/dynote-playground/src/frontend.tsx
@@ -1,6 +1,17 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+function App() {
+  return (
+    <div>
+      Playground goes here
+      <button>
+        Button
+      </button>
+    </div>
+  );
+}
+
 function start() {
   const root = createRoot(document.getElementById("root")!);
   root.render(<App />);
